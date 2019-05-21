8645d007300f9e67cea0c8d162e6711e2c8532b7
Theunis Coetzee <tcoetzee@c3r6s5.wethinkcode.co.za>
2019-05-21 14:07:35 +0200

    ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 14:07:11 by tcoetzee          #+#    #+#             */
+/*   Updated: 2019/05/21 14:07:14 by tcoetzee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const char	*sc;
+	size_t		i;
+
+	sc = (const char *)s;
+	i = -1;
+	while (++i < n)
+		if (*(sc + i) == (char)c)
+			return ((void *)sc + i);
+	return (NULL);
+}
