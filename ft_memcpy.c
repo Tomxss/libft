35c0c4806a131143a58bbe1de83ea7c2f4fd82a9
Theunis Coetzee <tcoetzee@c3r6s5.wethinkcode.co.za>
2019-05-21 14:11:44 +0200

    Create ft_memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 14:09:55 by tcoetzee          #+#    #+#             */
+/*   Updated: 2019/05/21 14:10:56 by tcoetzee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+	char	*ptr;
+	char	*ptr2;
+
+	ptr = dst;
+	ptr2 = (char *)src;
+	i = -1;
+	while (++i < n)
+		*(ptr + i) = *(ptr2 + i);
+	return (dst);
+}
