95cbaa3f80e485c13dc6901a117ed5af07bff6ff
Theunis Coetzee <tcoetzee@c3r6s5.wethinkcode.co.za>
2019-05-23 11:52:00 +0200

    Update Fatal error

--- ft_isupper.c
+++ ft_isupper.c
@@ -6,11 +6,11 @@
 /*   By: tcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 13:10:25 by tcoetzee          #+#    #+#             */
-/*   Updated: 2019/05/21 13:11:58 by tcoetzee         ###   ########.fr       */
+/*   Updated: 2019/05/23 11:51:41 by tcoetzee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft"
+#include "libft.h"
 
 int ft_isupper(int i)
 {
