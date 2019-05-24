73eea2ead353452c62b3c55e876fce989f81620c
Theunis Coetzee <tcoetzee@c3r6s5.wethinkcode.co.za>
2019-05-24 18:15:45 +0200

    Custom Aditional

---
+++ ft_strnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 17:41:00 by tcoetzee          #+#    #+#             */
+/*   Updated: 2019/05/24 17:43:17 by tcoetzee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * size + 1);
+	if (!str)
+		return (NULL);
+	ft_bzero(str, size + 1);
+	return (str);
+}
