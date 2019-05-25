0a07c74fc9d924c22ab57abbed8bfb55a6b5a643
Theunis Coetzee <tcoetzee@c3r6s5.wethinkcode.co.za>
2019-05-25 13:48:36 +0200

    Update libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 13:55:56 by tcoetzee          #+#    #+#             */
-/*   Updated: 2019/05/21 14:10:49 by tcoetzee         ###   ########.fr       */
+/*   Updated: 2019/05/24 19:19:05 by tcoetzee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,19 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 
-int		ft_strlen(char *str);
-
+char	*ft_strcpy(char *dest,const char *src);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strndup(const char *s1, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *big, const char *little);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_strlen(const char *s);
+char	*ft_strnew(size_t size);
 
 #endif
